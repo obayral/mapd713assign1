62acec53506b4b87e8d79f5dd75a99a7d5c2d0bc
OguzBayral <oguzbayral93@gmail.com>
2018-10-03 20:40:37 -0400

    Adjust to heroku host

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var restify = require('restify')
   // Create the restify server
   , server = restify.createServer({ name: SERVER_NAME})
 
-  server.listen(PORT, HOST, function () {
+  server.listen(PORT, function () {
   console.log('Server %s listening at %s', server.name, server.url)
   console.log('Resources:')
   console.log(' /products')
